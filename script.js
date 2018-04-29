7ae984821c0bb3b084ba43c410b875c73e621cf1
corey clippinger <coreyclip@gmail.com>
2018-04-28 17:24:00 -0700

    added base function for handling search button (not ready)

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+let $tbody = document.querySelector("tbody");
+let $CountryInput = document.querySelector("#country");
+let $dateInput = document.querySelector("#date");
+let $searchBtn = document.querySelector("#search");
+// Add an event listener to the searchButton, call handleSearchButtonClick when clicked
+$searchBtn.addEventListener("click", handleSearchButtonClick);
+
+function handleSearchButtonClick() {
+    
+    //let filterdate = $dateInput.value.trim().toLowerCase();
+
+    filteredDate = data.filter(function(date){
+        var addressState = address.state.toLowerCase();
+
+        return addressState === filterState;
+    });
+        renderTable();
+}
