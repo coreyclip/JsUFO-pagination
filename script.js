6042c163ee1493785946f87b22b4b4dcfeeccb6f
corey clippinger <coreyclip@gmail.com>
2018-04-29 15:42:17 -0700

    trying to build out table

--- script.js
+++ script.js
@@ -6,14 +6,43 @@ let $searchBtn = document.querySelector("#search");
 // Add an event listener to the searchButton, call handleSearchButtonClick when clicked
 $searchBtn.addEventListener("click", handleSearchButtonClick);
 
+let filteredRecords = data
+
+function renderTable(){
+    $tbody.innerHTML = "";
+    let len = filteredRecords.length
+    //let len = 100 //lets just do 100 rows for now
+    for (let i = 0; i < len; i++){
+        
+        //get records for row
+        let record = filteredRecords[i]
+        let fields = Object.keys(record)
+        
+        // make row in tbody
+        let $row = $tbody.insertRow(i)
+        
+        for (var j = 0; j < fields.length; j++){
+            //build out the row
+            let field = fields[j]
+            let $cell = $row.insertCell(j);
+            $cell.innnerText = record[field];
+        }
+    }
+}
+
+
 function handleSearchButtonClick() {
-    
-    //let filterdate = $dateInput.value.trim().toLowerCase();
-
+    // format user input
+    let filterDate = $dateInput.value.trim();
+    console.log("search for ", filterDate)
     filteredDate = data.filter(function(date){
-        var addressState = address.state.toLowerCase();
+        var RecordsDate = data.datetime.value;
 
-        return addressState === filterState;
+        return filterDate === RecordsDate;
     });
         renderTable();
+
 }
+
+// render table for first time load
+renderTable();
