b390280770f05e89e7456b737d12640d64c4ba43
corey clippinger <coreyclip@gmail.com>
2018-04-29 17:14:26 -0700

    changed to using "or" return funciton on search

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
 let $tbody = document.querySelector("tbody");
 let $stateInput = document.querySelector("#state");
 let $dateInput = document.querySelector("#date");
+let $cityInput = document.querySelector("#city");
+let $countryInput = document.querySelector("#country");
+let $shapeInput = document.querySelector("#shape");
+
 let $searchBtn = document.querySelector("#search");
+
+
+
 // Add an event listener to the searchButton, call handleSearchButtonClick when clicked
 $searchBtn.addEventListener("click", handleSearchButtonClick);
 
@@ -36,17 +43,28 @@ function handleSearchButtonClick() {
     // format user input
     let filterDate = $dateInput.value.trim().toLowerCase();
     let filterState = $stateInput.value.trim().toLowerCase();
-    
+    let filterCity = $cityInput.value.trim().toLowerCase();
+    let filterCountry = $countryInput.value.trim().toLowerCase();
+    let filterShape = $shapeInput.value.trim().toLowerCase();
     
     console.log("search for ", filterDate, filterState)
     
     filteredRecords = data.filter(function(record){
-        console.log(record.datetime)
-        var RecordsDate = record.datetime.toLowerCase();
+        
+        let RecordsDate = record.datetime.toLowerCase();
         let RecordsState = record.state.toLowerCase();
+        let RecordsCity = record.city.toLowerCase();
+        let RecordsCountry = record.country.toLowerCase();
+        let RecordsShape = record.shape.toLowerCase();
+        
+
+        //TO DO allow for one or more fields to be blank
 
         return RecordsDate === filterDate 
-        && RecordsState === filterState;
+        || RecordsState === filterState
+        || RecordsCity === filterCity
+        || RecordsCountry === filterCountry
+        || RecordsShape === filterShape;
     });
     renderTable();
 
